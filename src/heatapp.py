00deb5f2e3c97957594f559ae8af18ea3e81a47e
tejaskundu-bioscript <135111419+tejaskundu-bioscript@users.noreply.github.com>
2023-07-18 12:31:05 +0100

    Adding heatmap for deployment

---
+++ src/heatapp.py
@@ -0,0 +1,32 @@
+from dash import Dash, dcc, html, Input, Output
+import pandas as pd
+import plotly.express as px
+import os
+import plotly.graph_objects as go
+import dash_auth
+from flask import request
+
+
+def get_heatmap(df2):
+    figure = go.Figure(go.Densitymapbox(lat=df2['Latitude'], lon=df2['Longitude'], z=df2['Count'], radius=50))
+    figure.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+    figure.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    return figure
+
+
+df2 = pd.read_excel(os.getcwd() + "/../data/UK_Regions.xlsx")
+fig = get_heatmap(df2)
+
+app = Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    html.H4('HeatMap for UK Regions '),
+    html.Br(),
+    dcc.Graph(figure=fig),
+])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
